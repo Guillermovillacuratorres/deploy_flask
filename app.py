717f7dec3479707c21f30ce3e455ef97008eec91
Guillermo Villacura <Gu.villacura@gmail.com>
2025-06-15 18:29:11 -0400

    xd

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,  request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"status": "funcionando"})
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Verificar que es un push al branch correcto
+        data = request.json
+        if data['ref'] == 'refs/heads/main':
+            # Ejecutar el script de despliegue
+            subprocess.Popen(['powershell.exe', '-File', 'C:\\ruta\\a\\deploy.ps1'])
+            return jsonify({'status': 'success'}), 200
+    return jsonify({'status': 'ignored'}), 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
